5f2bccabe89aa00d19b47f2127fc1e8ab08459df
dulee <duckyounglee@oden.kr>
2022-06-21 16:42:31 +0900

    add main.ts log: env file path

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,18 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   console.log(`\t🚀 server run on port:\t${process.env.PORT}`);
   console.log(`\t🐱 mode:\t\t${process.env.NODE_ENV}`);
+  console.log(`\t👻 env file path:\t.env.${process.env.NODE_ENV}`);
   console.log(`\t🎮 enter with:\t\thttp://localhost:${process.env.PORT}`);
   console.log();
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidUnknownValues: true,
-    forbidNonWhitelisted: true,
-    transform: true,
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidUnknownValues: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(process.env.PORT);
 }
 bootstrap();
